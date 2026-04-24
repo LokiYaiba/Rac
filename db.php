6863cb92192369cb166c7d1f9dee1f68b303f8aa
LokiYaiba <aceladringan@gmail.com>
2026-04-24 09:24:27 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,6 @@
 <?php
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-$db   = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT') ?: 3306;
-
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+echo "HOST: " . getenv('MYSQLHOST') . "<br>";
+echo "USER: " . getenv('MYSQLUSER') . "<br>";
+echo "DB: " . getenv('MYSQLDATABASE') . "<br>";
+exit;
 ?>
